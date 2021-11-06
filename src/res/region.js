5bff343c96a622a8daa3c9f5a8884eaead529631
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-06 16:41:37 -0500

    region lib data

---
+++ src/res/region.js
@@ -0,0 +1,25 @@
+const region = [
+  // Parque del Perro
+  {
+    latitude: 3.435899466564378,
+    longitude: -76.5453948911183,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  },
+  // Tequendama
+  {
+    latitude: 3.419939563682522,
+    longitude: -76.54514740395723,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  },
+  // Santa Helena
+  {
+    latitude: 3.4300469666924025,
+    longitude: -76.52362668839255,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  },
+];
+
+export default region;
